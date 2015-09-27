97f30d4ebf7ce52fc4a7f2482b5b8d7debfd5f47
csidei <ideic@freemail.hu>
2015-09-27 16:12:45 +0200

    add some debug message

--- Client/Client.ino
+++ Client/Client.ino
@@ -15,9 +15,9 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  _devices[0] = DeviceFactory::GetDevice(LightSwitchDeviceType, NK1, 2);
-  _devices[1] = DeviceFactory::GetDevice(LightSwitchDeviceType, NK2, 3);
-  _devices[2] = DeviceFactory::GetDevice(LightSwitchDeviceType, NK3, 4);
+  _devices[0] = DeviceFactory::GetDevice(LightSwitchDeviceType, NK1, 4);
+  _devices[1] = DeviceFactory::GetDevice(LightSwitchDeviceType, NK2, 2);
+  _devices[2] = DeviceFactory::GetDevice(LightSwitchDeviceType, NK3, 3);
   
   _devices[3] = DeviceFactory::GetDevice(RelayDeviceType, NL1, 5);
   _devices[4] = DeviceFactory::GetDevice(RelayDeviceType, NL2, 6);
@@ -33,17 +33,22 @@ void loop() {
   for (int i = 0; i<DevicesNumber; i++) {
     if (_com.WeGotMessage(_devices[i]->GetAddress())) 
     {
-       Serial.println(F("We got Message"));
+      
        if (_com.WeGotGETMessage()) {
-         Serial.println(F("We got GET Message"));
          // in case of get
          bool isDeviceOn = _devices[i]->IsDeviceOn();
+         
          _com.SendMessage(_devices[i]->GetAddress(), _com.From(),   isDeviceOn ? MSG_DEVICE_ON : MSG_DEVICE_OFF);
        } 
        else if (_com.WeGotSETMessage()) {
-         Serial.println(F("We got SET Message"));
+         Serial.println("We got set message");
+
          _devices[i]->SetDeviceState(_com.SetMessageIsOn());
        }
+       else {
+           Serial.println("We got UNKNOWN message");        
+         }
+       
     }
   }
   delay(20);
