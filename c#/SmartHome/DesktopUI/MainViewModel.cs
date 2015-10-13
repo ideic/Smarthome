9a3ecc79fc849a8138553b54869218aa5971063a
csidei <ideic@freemail.hu>
2015-10-13 15:46:10 +0200

    add json serialization / deserialization Note add Edge brakes the ui

--- c#/SmartHome/DesktopUI/MainViewModel.cs
+++ c#/SmartHome/DesktopUI/MainViewModel.cs
@@ -1,40 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
+using Graphviz4Net.Dot;
+using Graphviz4Net.Dot.AntlrParser;
 using Graphviz4Net.Graphs;
+using Newtonsoft.Json;
 
 namespace DesktopUI
 {
     public class MainViewModel
     {
 
-        private Graph<Location> _graph;
+        private MyGraph<Location> _graph;
 
         public MainViewModel()
         {
-            _graph = new Graph<Location>();
+            _graph = new MyGraph<Location>();
 
 
             //_graph.AddVertex(new Location("Előszoba"));
             //_graph.AddVertex(new Location("Gépészet"));
-            var subGraph = new SubGraph<Location>(){Label = "Eloszoba"};
+            var subGraph = new MySubGraph<Location>(){Label = "Eloszoba"};
             _graph.AddSubGraph(subGraph);
 
             subGraph.AddVertex(new Switch("EK1"));
             subGraph.AddVertex(new Light("LEK1"));
 
-            _graph.AddEdge(new Edge<Location>(subGraph.Vertices.First(), subGraph.Vertices.Last(), new Arrow()));
+         //   _graph.AddEdge(new MyEdge<Location>(subGraph.Vertices.First(), subGraph.Vertices.Last(), new Arrow()));
 
-            var subGraph2 = new SubGraph<Location>(){Label = "Gepeszet"};
+            var subGraph2 = new MySubGraph<Location>(){Label = "Gepeszet"};
             _graph.AddSubGraph(subGraph2);
             subGraph2.AddVertex(new Switch("GK1"));
 
 
+            var binder = new TypeNameSerializationBinder("DesktopUI");
+      
+            var resString = JsonConvert.SerializeObject(_graph, Formatting.Indented, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto,
+                Binder = binder
+            });
+
+            
+            
+            //_graph = new MyGraph<Location>();
+
+
+            var deserialized = JsonConvert.DeserializeObject<MyGraph<Location>>(resString, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto,
+                Binder = binder
+            });
+
+
+            _graph = deserialized;
         }
 
-        public Graph<Location> Graph
+        public class TypeNameSerializationBinder : SerializationBinder
+        {
+            public string TypeFormat { get; private set; }
+
+            public TypeNameSerializationBinder(string typeFormat)
+            {
+                TypeFormat = typeFormat;
+            }
+
+            public override void BindToName(Type serializedType, out string assemblyName, out string typeName)
+            {
+                assemblyName = null;
+                typeName = serializedType.FullName;
+            }
+
+            public override Type BindToType(string assemblyName, string typeName)
+            {
+                return Type.GetType(typeName, true);
+            }
+        }
+
+
+        public MyGraph<Location> Graph
         {
             get { return _graph; }
         }
@@ -47,9 +96,112 @@ namespace DesktopUI
 
         public IEnumerable<string> LocationNames
         {
-            get { return Graph.AllVertices.Select(x => x.Name); }
+            get { return Graph.Vertices.Select(x => ((Location)x).Name); }
         }
     }
+
+    public class MyGraph<T> : IGraph
+    {
+        private ICollection<MySubGraph<T>> _subGraphs;
+        private ICollection<MyEdge<T>> _edges;
+        private ICollection<T> _vertices;
+
+        public MyGraph()
+        {
+            _edges = new List<MyEdge<T>>();
+            _vertices = new List<T>();
+            _subGraphs = new List<MySubGraph<T>>();
+
+        }
+        public IEnumerable<IEdge> Edges {
+            get { return _edges; }
+        }
+
+        public IEnumerable<object> Vertices {
+            get { return _vertices.Cast<object>(); }
+        }
+
+        public IEnumerable<ISubGraph> SubGraphs {
+            get { return _subGraphs; }
+        }
+
+        public event EventHandler<GraphChangedArgs> Changed;
+
+        public void AddSubGraph(MySubGraph<T> subGraph)
+        {
+            _subGraphs.Add(subGraph);
+        }
+
+        public void AddEdge(MyEdge<T> edge)
+        {
+            _edges.Add(edge);
+        }
+    }
+
+    public class MyEdge<T> : IEdge<T>
+    {
+        public MyEdge(T source, T destination, Arrow arrow)
+        {
+            Source = source;
+            Destination = destination;
+            DestinationArrow = arrow;
+        }
+
+        object IEdge.Source
+        {
+            get { return Source; }
+        }
+
+        public T Destination { get; set; }
+
+        public T Source { get; set; }
+
+        object IEdge.Destination
+        {
+            get { return Destination; }
+        }
+
+        public object DestinationPort
+        {
+             get; set; 
+        }
+
+        public object DestinationArrow { get; private set; }
+
+        public object SourceArrow { get; set; }
+
+        public object SourcePort { get; set; }
+    }
+
+    public class MySubGraph<T> : ISubGraph<T>
+    {
+        private ICollection<T> _vertices;
+        public MySubGraph()
+        {
+            _vertices = new List<T>();
+        }
+        
+        public IEnumerable<T> Vertices {
+            get {  return _vertices; }
+        }
+
+        IEnumerable<object> ISubGraph.Vertices
+        {
+            get { return _vertices.Cast<object>(); }
+        }
+
+        public string Label { get; set; }
+
+        public event EventHandler<GraphChangedArgs> Changed;
+
+        public void AddVertex(T vertex)
+        {
+            _vertices.Add(vertex);
+        }
+    }
+
+
+
 
     public class Location
     {
